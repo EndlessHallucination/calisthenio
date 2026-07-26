31e4fca11f90f4c9f5b88360c1cb2e02d27daadb
basil <basiln@icloud.com>
2026-07-26 10:27:46 +0300

    some changes

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -4,4 +4,17 @@ const client = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 404) {
+      return Promise.resolve({ data: null });
+    }
+    if (error.response?.status === 500) {
+      console.error("Server error:", error.response.data);
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default client;
